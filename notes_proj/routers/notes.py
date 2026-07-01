01cc04bc147a129c9c148551b0cb977a25913a42
anish <anish00924@gmail.com>
2026-07-01 16:54:13 +0530

    new change in patch while allowing editing

--- notes_proj/routers/notes.py
+++ notes_proj/routers/notes.py
@@ -69,16 +69,17 @@ async def acess(id:int,token:str):
     note=await database.fetch_one(query)
     return note
 @router.patch("/notes/{id}/shared/{token}")
-async def editing(id:int,token:str,note:patchIN):
+async def editing(id:int,token:str,note:patchIN,current_user:Annotated[User,Depends(get_current_user)]):
     edit=await get_shared_edit(token,"shared")
     if not edit:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="edit permissions not given")
-    data=note.model_dump()
-    q=note_table.select().where(note_table.c.id==data['id'])
+    data=note.model_dump(exclude_unset=True) #all the values in the pydantic model that are optional and not sent by the client or sent as null will be ignored and not unpacked as say title:Null
+    note_id=data.pop('id') #as we arent explicitly setting the key value like body=data['body'] and we just pass in the dict to the values for automatic kwargs matching so it includes id hence it tries to set id we dont want that as id is aldready existing and need not be changed hence pop it out
+    q=note_table.select().where(and_(note_table.c.id==note_id,note_table.c.user_id==current_user.id))
     n=await database.fetch_one(q)
     if n is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="note not found")
-    query=note_table.update().where(note_table.c.id==data['id']).values(body=data['body'])
+    query=note_table.update().where(note_table.c.id==note_id).values(**data)
     response=await database.execute(query)
     if response is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="note not found")
